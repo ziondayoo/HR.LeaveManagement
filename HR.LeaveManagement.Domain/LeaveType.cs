ffda523b3f2e8fd1a635e43006ba2127147f4058
Adetunji Temidayo <adetunjitemidayo26@gmail.com>
2024-04-09 16:58:15 +0100

    Add project files.

---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Domain.Common;
+using System;
+
+namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType : BaseDomainEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+    }
+}
